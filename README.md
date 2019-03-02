96e47057bbcfe80ee599f44bcd98f57b51ccc328
Fangzhou Xu <fangzxu@gmail.com>
2019-03-02 00:13:20 -0800

    better error handling (#14)

--- README.md
+++ README.md
@@ -17,29 +17,42 @@ main() async {
   EOSClient client = EOSClient('https://eos.greymass.com', 'v1');
 
   // Get EOS Node Info
-  await client.getInfo().then((NodeInfo nodeInfo) {
+  client.getInfo().then((NodeInfo nodeInfo) {
     print(nodeInfo);
   });
 
+  // Get EOS Abi
+  client.getAbi('eosio.token').then((Abi abi) {
+    print(abi);
+  });
+
+  // Get EOS Raw Abi
+  client.getRawAbi('eosio.token').then((Abi abi) {
+    print(abi);
+  });
+
+  // Get EOS Raw code and Abi
+  client.getRawCodeAndAbi('eosio.token').then((Abi abi) {
+    print(abi);
+  });
+
   // Get EOS Block Info
-  await client.getBlock('351').then((Block block) {
+  client.getBlock('351').then((Block block) {
     print(block);
   });
 
   // Get Account Info
-  await client.getAccount('eosio.stake').then((Account account) {
+  client.getAccount('eosio.stake').then((Account account) {
     print(account);
   });
 
   // Get Account Actions
-  await client
-      .getActions('eosio.stake', pos: -1, offset: -1)
-      .then((Actions actions) {
+  client.getActions('eosio.stake', pos: -1, offset: -1).then((Actions actions) {
     print(actions);
   });
 
   // Get Transaction
-  await client
+  client
       .getTransaction(
           '83875faeb054ba20b20f392418e3a0002c4bb1c36cc4e3fde15cbd0963da8a15')
       .then((Transaction transaction) {
@@ -47,10 +60,17 @@ main() async {
   });
 
   // Get Accounts from public key
-  await client
+  client
       .getKeyAccounts('EOS8RWQpzzMi5uFXXXAChi4dHnyxMYKKdAQ3Y3pHQTrvhzGk95LbT')
       .then((AccountNames accountNames) {
     print(accountNames);
+  });
+
+  // Get currency balance
+  client
+      .getCurrencyBalance('parslseed123', 'newdexpocket')
+      .then((List<Holding> balance) {
+    print(balance);
   });
 }
 ```
--- lib/src/eosdart_base.dart
+++ lib/src/eosdart_base.dart
@@ -26,7 +26,7 @@ class EOSClient {
             body: json.encode(body))
         .then((http.Response response) {
       if (response.statusCode != 200) {
-        throw response.body;
+        completer.completeError(response.body);
       } else {
         completer.complete(json.decode(response.body));
       }
@@ -36,113 +36,92 @@ class EOSClient {
 
   /// Get EOS Node Info
   Future<NodeInfo> getInfo() async {
-    Completer<NodeInfo> completer = Completer();
-    this._post('/chain/get_info', {}).then((nodeInfo) {
-      completer.complete(NodeInfo.fromJson(nodeInfo));
+    return this._post('/chain/get_info', {}).then((nodeInfo) {
+      return NodeInfo.fromJson(nodeInfo);
     });
-    return completer.future;
   }
 
   /// Get EOS Block Info
   Future<Block> getBlock(String blockNumOrId) async {
-    Completer<Block> completer = Completer();
-    this._post('/chain/get_block', {'block_num_or_id': blockNumOrId}).then(
-        (block) {
-      completer.complete(Block.fromJson(block));
+    return this._post(
+        '/chain/get_block', {'block_num_or_id': blockNumOrId}).then((block) {
+      return Block.fromJson(block);
     });
-    return completer.future;
   }
 
   /// Get EOS Block Header State
   Future<BlockHeaderState> getBlockHeaderState(String blockNumOrId) async {
-    Completer<BlockHeaderState> completer = Completer();
-    this._post('/chain/get_block_header_state',
+    return this._post('/chain/get_block_header_state',
         {'block_num_or_id': blockNumOrId}).then((block) {
-      completer.complete(BlockHeaderState.fromJson(block));
+      return BlockHeaderState.fromJson(block);
     });
-    return completer.future;
   }
 
   /// Get EOS abi from account name
   Future<Abi> getAbi(String accountName) async {
-    Completer<Abi> completer = Completer();
-    this._post('/chain/get_abi', {'account_name': accountName}).then((abi) {
-      completer.complete(Abi.fromJson(abi));
+    return this
+        ._post('/chain/get_abi', {'account_name': accountName}).then((abi) {
+      return Abi.fromJson(abi);
     });
-    return completer.future;
   }
 
   /// Get EOS raw abi from account name
   Future<Abi> getRawAbi(String accountName) async {
-    Completer<Abi> completer = Completer();
-    this._post('/chain/get_raw_abi', {'account_name': accountName}).then((abi) {
-      completer.complete(Abi.fromJson(abi));
+    return this
+        ._post('/chain/get_raw_abi', {'account_name': accountName}).then((abi) {
+      return Abi.fromJson(abi);
     });
-    return completer.future;
   }
 
   /// Get EOS raw code and abi from account name
   Future<Abi> getRawCodeAndAbi(String accountName) async {
-    Completer<Abi> completer = Completer();
-    this._post('/chain/get_raw_code_and_abi',
+    return this._post('/chain/get_raw_code_and_abi',
         {'account_name': accountName}).then((abi) {
-      completer.complete(Abi.fromJson(abi));
+      return Abi.fromJson(abi);
     });
-    return completer.future;
   }
 
   /// Get EOS account info form the given account name
   Future<Account> getAccount(String accountName) async {
-    Completer<Account> completer = Completer();
-    this._post('/chain/get_account', {'account_name': accountName}).then(
+    return this._post('/chain/get_account', {'account_name': accountName}).then(
         (account) {
-      completer.complete(Account.fromJson(account));
+      return Account.fromJson(account);
     });
-    return completer.future;
   }
 
   /// Get EOS account info form the given account name
   Future<List<Holding>> getCurrencyBalance(String code, String account,
       [String symbol]) async {
-    Completer<List<Holding>> completer = Completer();
-    this._post('/chain/get_currency_balance',
+    return this._post('/chain/get_currency_balance',
         {'code': code, 'account': account, 'symbol': symbol}).then((balance) {
-      completer.complete(
-          (balance as List).map((e) => new Holding.fromJson(e)).toList());
+      return (balance as List).map((e) => new Holding.fromJson(e)).toList();
     });
-    return completer.future;
   }
 
   /// Get EOS account actions
   Future<Actions> getActions(String accountName, {int pos, int offset}) async {
-    Completer<Actions> completer = Completer();
-    this._post('/history/get_actions', {
+    return this._post('/history/get_actions', {
       'account_name': accountName,
       'pot': pos,
       'offset': offset
     }).then((actions) {
-      completer.complete(Actions.fromJson(actions));
+      return Actions.fromJson(actions);
     });
-    return completer.future;
   }
 
   /// Get EOS transaction
   Future<Transaction> getTransaction(String id, {int blockNumHint}) async {
-    Completer<Transaction> completer = Completer();
-    this._post('/history/get_transaction',
+    return this._post('/history/get_transaction',
         {'id': id, 'block_num_hint': blockNumHint}).then((transaction) {
-      completer.complete(Transaction.fromJson(transaction));
+      return Transaction.fromJson(transaction);
     });
-    return completer.future;
   }
 
   /// Get Key Accounts
   Future<AccountNames> getKeyAccounts(String pubKey) async {
-    Completer<AccountNames> completer = Completer();
-    this._post('/history/get_key_accounts', {'public_key': pubKey}).then(
+    return this._post('/history/get_key_accounts', {'public_key': pubKey}).then(
         (accountNames) {
-      completer.complete(AccountNames.fromJson(accountNames));
+      return AccountNames.fromJson(accountNames);
     });
-    return completer.future;
   }
 }
